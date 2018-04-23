1a12406512783c6d0758e600a3bedfc3743006f5
1gnatov <ignatov.msu@gmail.com>
2018-04-23 23:37:25 +0300

    added autocomplete property and condition on property

--- iron-starter/src/main/java/com/ironbank/IronConfiguration.java
+++ iron-starter/src/main/java/com/ironbank/IronConfiguration.java
@@ -1,13 +1,18 @@
 package com.ironbank;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(RavenProperties.class)
 public class IronConfiguration {
 
 	@Bean
 	@ConditionalOnProduction
+	@ConditionalOnProperty("raven.destinations")
 	public IronListener ironListener() {
 		return new IronListener();
 	}
--- iron-starter/src/main/java/com/ironbank/IronListener.java
+++ iron-starter/src/main/java/com/ironbank/IronListener.java
@@ -1,14 +1,21 @@
 package com.ironbank;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 @Slf4j
 public class IronListener implements ApplicationListener<ContextRefreshedEvent> {
+	private RavenProperties ravenProperties;
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		log.info("Посылаем ворона....");
+		ravenProperties.getDestinations().forEach(d -> log.info("Send raven to " + d));
+	}
+
+	@Autowired
+	public void setRavenProperties(RavenProperties ravenProperties) {
+		this.ravenProperties = ravenProperties;
 	}
 }
